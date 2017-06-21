f7ce4789faf08b8e8ef63e24520bec824aaa2ed8
dongzhihan <529211933@qq.com>
2017-06-21 17:15:07 +0800

    first commit

---
+++ src/components/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    你好 马哥
+  </div>
+</template>
+
+<script>
+
+  // import api from '../js/api';
+
+  export default {
+    data() {
+      return {
+
+      };
+    },
+    components: {
+
+    },
+    methods: {
+
+    }
+  };
+
+</script>
+<style scoped>
+  .fontName {
+    font-size: 0.5rem;
+    font-weight: bold;
+    font-family: STXihei, "华文细黑", "Microsoft YaHei", "微软雅黑";
+  }
+
+</style>
